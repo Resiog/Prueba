9ffe6dd4bcb952b566e7bc50f62d91d39144d1c5
Sergio <sermedproject@gmail.com>
2025-03-23 13:17:56 +0100

    Creado git

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box (modifier = Modifier.padding(innerPadding)) {
+                        Greeting(
+                            name = "Android"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello $name!"
+    )
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Box (modifier = Modifier.padding(innerPadding)) {
+                Greeting(
+                    name = "Android"
+                )
+            }
+        }
+    }
+}
